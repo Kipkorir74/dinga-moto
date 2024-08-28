c672b3ac7262c943f9217768962568c31acaac2e
Kipkorir74 <grahamhim74@gmail.com>
2024-08-28 16:30:57 +0300

    search manufacturer

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,15 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: string;
-    containerStyle: string;
-    handleClick?: MouseEventHandler<HTMLButtonElement>;
-    btnType?: "button" | "submit" | "reset";
-  }
-  
-  
+  title: string;
+  containerStyle: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit" | "reset";
+}
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+
+}
+
