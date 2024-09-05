070401581463dc807da354924f0f08ed7df2acbb
Kipkorir74 <grahamhim74@gmail.com>
2024-09-05 12:46:09 +0300

    list cars

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,9 +1,11 @@
-"use Client"
+"use client"
 import { useState } from 'react';
 import Image from 'next/image';
 
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
+import { calculateCarRent } from '@/utils';
+import CarDetails from './CarDetails';
 
 interface CarCardProps {
     car: CarProps;
@@ -11,6 +13,8 @@ interface CarCardProps {
 
 const CarCard = ({ car }: CarCardProps) => {
     const { city_mpg, class: carClass, combination_mpg, cylinders, displacement, drive, fuel_type, highway_mpg, make, model, transmission, year } = car;
+    const carRent = calculateCarRent(city_mpg, year);
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className='car-card group'>
@@ -19,11 +23,51 @@ const CarCard = ({ car }: CarCardProps) => {
                     {make} {model}
                 </h2>
             </div>
-            <p>
-                <span>
-                    Car rent...
+            <p className='flex mt-6 text-[32px] font-extrabold'>
+                <span className='self-start text-[14px] font-semibold'>
+                    $
+                </span>
+                {carRent}
+                <span className='self-end text-[14px] font-medium'>
+                    /day
                 </span>
             </p>
+            <div className="relative w-full h-40 my-3 object-contain">
+                <Image src="/hero.png" alt='car model' fill priority className='' />
+            </div>
+            <div className='relative flex w-full mt-2'>
+                <div className='flex group-hover:invisible w-full justify-between text-gray'>
+                    <div className='flex flex-col justify-center items-center gap-2'>
+                        <Image src="/steering-wheel.svg" alt='steering-wheel' width={20} height={20} />
+                        <p className='text-[14px]'>
+                            {transmission === 'automatic' ? 'Auto' : 'Manual'}
+                        </p>
+                    </div>
+                    <div className='flex flex-col justify-center items-center gap-2'>
+                        <Image src="/tire.svg" alt='tire' width={20} height={20} />
+                        <p className='text-[14px]'>
+                            {drive.toUpperCase()}
+                        </p>
+                    </div>
+                    <div className='flex flex-col justify-center items-center gap-2'>
+                        <Image src="/gas.svg" alt='steering-wheel' width={20} height={20} />
+                        <p className='text-[14px]'>
+                            {city_mpg}
+                        </p>
+                    </div>
+                </div>
+                <div className='car-card__btn-container'>
+                    <CustomButton
+                        title='Rent'
+                        containerStyle='w-full py-[16px] rounded-full bg-primary-blue'
+                        textStyles='text-white text-[14px] leading-[16px] font-bold'
+                        rightIcon='/right-arrow.svg'
+                        handleClick={()=>setIsOpen(true)}
+                    />
+                </div>
+            </div>
+            <CarDetails isOpen={isOpen} closeModal={()=>
+                setIsOpen{(false)} car={car}}/>
         </div>
     )
 }
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,7 @@
 "use client";//turn component into client side component
 import Image from "next/image";
 import { CustomButtonProps } from "@/types";
-const CustomButton = ({title, containerStyle, handleClick, btnType}:CustomButtonProps) => {
+const CustomButton = ({title, containerStyle, handleClick, btnType, textStyles, rightIcon}:CustomButtonProps) => {
     return (
         <button
             disabled={false}
@@ -9,9 +9,17 @@ const CustomButton = ({title, containerStyle, handleClick, btnType}:CustomButton
             className={`custom-btn ${containerStyle}`}
             onClick={handleClick}
         >
-            <span className={`flex-1`}>
+            <span className={`flex-1 ${textStyles}`}>
                 {title}
             </span>
+            {rightIcon && (
+                <div className="relative w-6 h-6">
+                    <Image src={rightIcon} alt="right icon" 
+                    layout="fill" 
+                    className="object-contain"
+                    />
+                </div>
+            )}
         </button>
     )
 }
