399fa28fbe32704a4a11944cc036383c1a0ec688
Kipkorir74 <grahamhim74@gmail.com>
2024-09-10 15:58:54 +0300

    add search icon

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,21 +2,46 @@
 
 import SearchManufacturer from "./SearchManufacturer"
 import { useState } from 'react'
+import Image from 'next/image'
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image src='/magnifying-glass.svg ' alt='magnifying glass' width={40} height={40}
+      className='object-contain'
+    />
+  </button>
+)
 
 const SearchBar = () => {
-    const [manufacturer, setManufacturer] = useState('')
+  const [manufacturer, setManufacturer] = useState('');
+  const [model, setModel] = useState('');
 
-    const handleSeacrh = () => {}
+  const handleSeacrh = () => { }
 
   return (
     <form className='searchbar' onSubmit={handleSeacrh}>
-        <div className="searchbar__item">
-            <SearchManufacturer 
-                manufacturer={manufacturer}
-                setManufacturer={setManufacturer}
-            />
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
 
-        </div>
+      <div className="searchbar__item">
+        <Image src='/model-icon.png' alt='model-icon'
+          width={25} height={25} className='absolute w-[20px] h-[20px] ml-4'
+        />
+        <input type='text'
+          name='model'
+          value={model}
+          className='searchbar__input' 
+          onChange={(e)=>setModel(e.target.value)}
+          placeholder="Tiguan"
+          />
+          <SearchButton otherClasses="sm:hidden" />
+          <SearchButton otherClasses="max-sm:hidden" />
+      </div>
     </form>
   )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:['cdn.imagin.studio'],
+    images: {
+        domains: ['cdn.imagin.studio'],
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     }
 };
 
