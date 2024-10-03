25cc1d8b11339aaa54cce527060850eb70520f22
Kipkorir74 <grahamhim74@gmail.com>
2024-10-03 17:04:27 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
 
   return (
     <main className="overflow-hidden">
-      <Hero />
+      <Hero title={""} containerStyle={""} />
 
       <div className="mt-12 padding-x padding-y 
       max-width id='discover'">
