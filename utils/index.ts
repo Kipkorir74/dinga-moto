222daf2419b74ed14e336fc1ffcc8730c1b35aca
Kipkorir74 <grahamhim74@gmail.com>
2024-10-03 11:19:40 +0300

    Filters update

--- utils/index.ts
+++ utils/index.ts
@@ -47,7 +47,7 @@ export const updateSearchParams = (type: string, value: string) => {
     const searchParams = new URLSearchParams(window.location.search);
 
 
-    searchParams.set('type', value);
+    searchParams.set(type, value);
 
     const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
     return newPathName;
